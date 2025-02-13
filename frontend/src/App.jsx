7ea3fae7a9a31b8bdb740aee9371c11c88d6b227
pedroosorio <pedro.osorio04@inacapmail.cl>
2025-02-13 17:50:06 -0300

    inicio Blog con DRF y React, lado del frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+//import { Button } from "./components/ui/button"
+import Header from "./ui/Header"
+import Navbar from "./ui/Navbar"
+import BlogContainer from "./ui/BlogContainer"
+import Footer from "./ui/Footer"
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <BlogContainer />
+    <Footer />
+
+    
+
+
+    </>
+  )
+}
+
+export default App
